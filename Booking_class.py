0148b933daafd22dfac1e1919bac777e416cd195
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-17 15:14:11 +0000

    fixed the seat_counter method

--- Booking_class.py
+++ Booking_class.py
@@ -8,11 +8,10 @@ class Booking:
     def __init__(self, total_seats):
         self.available_seats = total_seats
         self.ticket_price = 100
-        self.order_total = 0
         self.total_tickets = 0
-        self.adult_tickets = 0
-        self.child_tickets = 0
-        self.lap_child_tickets = 0
+        # self.adult_tickets = 0
+        # self.child_tickets = 0
+        # self.lap_child_tickets = 0
 
 
     def validation_check(self):
@@ -23,41 +22,44 @@ class Booking:
             return
 
     def total_cost(self):
+        tickets_ordered = 0
+        order_total = 0
         adult_tickets = int(input("How many adult tickets would you like to purchase?    "))
         child_tickets = int(input("How many child tickets (ages 2-14) would you like to purchase?    "))
         lap_child_tickets = int(input("How many lap child tickets (ages 0-2) would you like to purchase?    "))
 
         if adult_tickets > 0:
-            self.order_total += (adult_tickets * self.ticket_price)
+            order_total += (adult_tickets * self.ticket_price)
+            tickets_ordered += adult_tickets
             self.total_tickets += adult_tickets
-            self.adult_tickets += adult_tickets
         if child_tickets > 0:
-            self.order_total += (child_tickets * (self.ticket_price * 0.75))
+            order_total += (child_tickets * (self.ticket_price * 0.75))
+            tickets_ordered += child_tickets
             self.total_tickets += child_tickets
-            self.child_tickets += child_tickets
         if lap_child_tickets > 0:
-            self.order_total += (lap_child_tickets * (self.ticket_price * 0.3))
+            order_total += (lap_child_tickets * (self.ticket_price * 0.3))
+            tickets_ordered += lap_child_tickets
             self.total_tickets += lap_child_tickets
-            self.lap_child_tickets += lap_child_tickets
-        print(f"You have ordered {self.total_tickets} tickets: \n"
+
+        print(f"\nYou have ordered {tickets_ordered} tickets: \n"
         f"{adult_tickets} Adult tickets \n"
         f"{child_tickets} Child tickets \n"
         f"{lap_child_tickets} Lap Child tickets \n"
-        f"Your order total is £{self.order_total} \n")
+        f"Your order total is £{order_total} \n")
 
         confirmation = input("Would you like to continue with the purchase? (Y/N)    ")
         if confirmation == "y":
-            self.seat_counter()
-            self.order_details()
+            self.seat_counter(adult_tickets, child_tickets)
+            # self.order_details()
         elif confirmation == "n":
             return
 
-    def seat_counter(self):
-        if self.adult_tickets > 0:
-            self.available_seats -= self.adult_tickets
-        if self.child_tickets > 0:
-            self.available_seats -= self.child_tickets
-        return print(f"There are {self.available_seats} seats remaining on the flight")
+    def seat_counter(self, adult_tickets, child_tickets):
+        if adult_tickets > 0:
+            self.available_seats -= adult_tickets
+        if child_tickets > 0:
+            self.available_seats -= child_tickets
+        return print(f"There are {self.available_seats} seats remaining on the flight \n \n")
 
 
     def order_details(self):
@@ -69,4 +71,5 @@ class Booking:
         conn.commit()
 
 test = Booking(400)
-test.validation_check()
+while True:
+    test.validation_check()
