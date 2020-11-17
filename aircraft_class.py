57436637c4ea69ff5fcc857f2816d40c5e0cc265
Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
2020-11-17 14:59:34 +0000

    teabreak commit

--- aircraft_class.py
+++ aircraft_class.py
@@ -10,21 +10,21 @@ class Aircraft(Connection):
 
 # Create methods for helicopter and plane
     def plane(self):
-        type=["A380","B777","A320neo"]
+        craft_type=["A380","B777","A320neo"]
         capacity=[517,364,160]
-        classes =[3,3,2]
+        travel_class =[3,3,2]
 
     def helicopter(self):
         pass
 
     def create_table(self):
         # Create a table in the database to store aircraft data
-        crafts=self.cursor.execute("CREATE TABLE aircrafts (craft_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,Type VARCHAR(20), Model VARCHAR(55), Capacity INT, Num_Classes INT ")
+        crafts=self.cursor.execute("CREATE TABLE aircraft (craft_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,Type VARCHAR(20), Model VARCHAR(55), Capacity INT, Num_Classes INT ")
 
     def insert_data(self):
         # Insert data into the crafts table
         self.cursor.execute(
-            f"INSERT INTO aircrafts (Type, Model, Capacity, Num_Classes) VALUES ('plane', '{type[0]}', '{}','{adult}','{start}','{end}','{runtime}','{genre}')")
+            f"INSERT INTO aircraft (Type, Model, Capacity, Num_Classes) VALUES ('plane', '{craft_type[0]}','{capacity[0]}','{travel_class[0]}')")
 
 
 
