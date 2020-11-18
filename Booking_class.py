a24124874e9b9ba7b9b9e9bd452801ee035975b6
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-18 14:28:56 +0000

    Ben (#15)
    * branch ben
    
    * donovan branch
    
    * readme.md
    
    * Re-named README with step 1
    
    * booking class iteration 1
    
    * DB connection class
    
    * fixed the seat_counter method
    
    * new gitignore
    
    * error handling in booking_class.py
    
    * Quick change on tax_number not needed on employees
    
    * print method to show what is inside a table within SQL database
    
    * Basic customer class
    
    * Removed deleted stuff from directory
    
    * Added clarification print statements, test connect
    
    * Added bit about installing pyodbc
    
    * started user_functions class
    
    * Created the adding data to customer table
    
    * Fixed data connections
    
    * Add user_interface file, with comments
    
    * Removed infant stuff, moved to booking class
    
    * Finished the customer class, need show customers
    
    * Added some comments
    
    * Added comments
    
    * Finished employee class
    
    * Added show all customers class
    
    * removed .csv files
    
    * pulling
    
    * Ben (#11) (#12)
    
    * branch ben
    
    * readme.md
    
    * booking class iteration 1
    
    * fixed the seat_counter method
    
    * error handling in booking_class.py
    
    * print method to show what is inside a table within SQL database
    
    * started user_functions class
    
    Co-authored-by: Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
    
    Co-authored-by: Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
    
    Co-authored-by: Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
    Co-authored-by: Donovan Gervais <applemac@Donovans-MacBook-Pro-7.local>
    Co-authored-by: Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
    Co-authored-by: AmaanHUB <73166293+AmaanHUB@users.noreply.github.com>

--- Booking_class.py
+++ Booking_class.py
@@ -3,12 +3,21 @@
 # Cost method
 # Seat counter (See how many seats are available. If seat is sold, subtract from seats available before.)
 
+from db_connection_class import DB_Connection
+from customer_class import Customer
+import pandas as pd
 
-class Booking:
-    def __init__(self):
+
+class Booking(DB_Connection, Customer)
+    def __init__(self, adult_tickets, child_tickets, infant_tickets):
+        super().__init__()
         self.available_seats = 400
         self.ticket_price = 100
         self.total_tickets = 0
+        self.adult_tickets = adult_tickets
+        self.child_tickets = child_tickets
+        self.infant_tickets = infant_tickets
+        self.order_total = 0
 
     def validation_check(self):
         if self.available_seats == 0:
@@ -22,29 +31,19 @@ class Booking:
     def total_cost(self):
         print(f"There are currently {self.available_seats} seats remaining")
         tickets_ordered = 0
-        order_total = 0
-        try:
-            adult_tickets = int(input("How many adult tickets would you like to purchase?    "))
-            child_tickets = int(input("How many child tickets (ages 2-14) would you like to purchase?    "))
-            lap_child_tickets = int(input("How many lap child tickets (ages 0-2) would you like to purchase?    "))
-        except ValueError as err:
-            print("Please Enter a Valid Number")
-            return
-        else:
-            pass
 
-        if adult_tickets > 0:
-            order_total += (adult_tickets * self.ticket_price)
-            tickets_ordered += adult_tickets
-            self.total_tickets += adult_tickets
-        if child_tickets > 0:
-            order_total += (child_tickets * (self.ticket_price * 0.75))
-            tickets_ordered += child_tickets
-            self.total_tickets += child_tickets
-        if lap_child_tickets > 0:
-            order_total += (lap_child_tickets * (self.ticket_price * 0.3))
-            tickets_ordered += lap_child_tickets
-            self.total_tickets += lap_child_tickets
+        if self.adult_tickets > 0:
+            self.order_total += (self.adult_tickets * self.ticket_price)
+            tickets_ordered += self.adult_tickets
+            self.total_tickets += self.adult_tickets
+        if self.child_tickets > 0:
+            self.order_total += (self.child_tickets * (self.ticket_price * 0.75))
+            tickets_ordered += self.child_tickets
+            self.total_tickets += self.child_tickets
+        if self.infant_tickets > 0:
+            self.order_total += (self.infant_tickets * (self.ticket_price * 0.3))
+            tickets_ordered += self.infant_tickets
+            self.total_tickets += self.infant_tickets
 
         try:
             if tickets_ordered > self.available_seats:
@@ -56,15 +55,15 @@ class Booking:
             pass
 
         print(f"\nYou have ordered {tickets_ordered} tickets: \n"
-              f"{adult_tickets} Adult tickets \n"
-              f"{child_tickets} Child tickets \n"
-              f"{lap_child_tickets} Lap Child tickets \n"
-              f"Your order total is £{order_total} \n")
+              f"{self.adult_tickets} Adult tickets \n"
+              f"{self.child_tickets} Child tickets \n"
+              f"{self.infant_tickets} Lap Child tickets \n"
+              f"Your order total is £{self.order_total} \n")
 
         confirmation = input("Would you like to continue with the purchase? (Y/N)    ")
         if confirmation == "y":
-            self.seat_counter(adult_tickets, child_tickets)
-            # self.order_details()
+            self.seat_counter(self.adult_tickets, self.child_tickets)
+            self.order_details(self.adult_tickets, self.child_tickets, self.infant_tickets, self.order_total)
         else:
             return
 
@@ -75,11 +74,24 @@ class Booking:
             self.available_seats -= child_tickets
         return print(f"There are {self.available_seats} seats remaining on the flight \n \n")
 
-    # def order_details(self):
-    # cursor.execute(f"""INSERT INTO #booking table#
-    #                         (OrderID, FlightID, lap_child_tickets, child_tickets, adult_tickets, total_price)
-    #                 VALUES
-    #                         (OrderID, FlightID, {self.lap_child_tickets}, {self.child_tickets}, {self.adult_tickets}, {self.order_total})
-    #                 """)
-    # conn.commit()
+    def order_details(self, adult_tickets, child_tickets, infant_tickets, order_total):
 
+        self.cursor.execute(f"""INSERT INTO #booking table#
+                                (OrderID, FlightID, infant_tickets, child_tickets, adult_tickets, total_price)
+                        VALUES
+                                (OrderID, FlightID, {infant_tickets}, {child_tickets}, {adult_tickets}, {order_total})
+                        """)
+        self.connection.commit()
+
+        exported_data = pd.read_sql_query(
+            f'SELECT *  FROM #booking_table# WHERE First_name = {self.first_name}',
+            self.connection)
+        df = pd.DataFrame(exported_data)
+
+        "Here are the details of your order \n" \
+        f"{df}"
+
+    def amend_booking(self):
+        self.cursor.execute(
+            f'DELETE FROM #bookingtable# where first_name = {self.first_name}')
+        print("The booking has been deleted")
