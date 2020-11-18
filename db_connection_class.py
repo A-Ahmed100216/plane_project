7fcaed8d0805f033a7b1aa2c6444f10b27a88b11
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-18 10:20:58 +0000

    Ben (#5)
    * branch ben
    
    * readme.md
    
    * booking class iteration 1
    
    * fixed the seat_counter method
    
    * error handling in booking_class.py
    
    * print method to show what is inside a table within SQL database
    
    * started user_functions class
    
    Co-authored-by: Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>

---
+++ db_connection_class.py
@@ -0,0 +1,21 @@
+import pyodbc  # Import pyodbc so that you can connect to a database
+
+
+# Create a class that connects to the database
+class DB_Connection():
+    def __init__(self):
+        self.server = "databases1.spartaglobal.academy"
+        self.database = "Northwind"  # the name of our newly created database
+        self.username = "SA"
+        self.password = "Passw0rd2018"
+        # establish connection
+        self.connection = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
+
+        self.cursor = self.connection.cursor()
+
+
+# create and instance of this class to test the connection
+if __name__ == "__main__":
+    test = DB_Connection()
+    test.__init__()
