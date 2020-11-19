e7b5c5829dddb67956dcc253f193da047f006e13
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-11-19 21:32:12 +0000

    Added some small pseudocode

--- customers_class.py
+++ customers_class.py
@@ -1,13 +1,15 @@
 from people_class import People
 from db_connection import DB_Connection
 
-# Class for the Customers
+# Class for the Customers that inherits from People
 class Customer(People):
+   # initialise the class
     def __init__(self, passport_number, first_name, surname, tax_number):
+        # inherit these variables from the people class
         super().__init__(passport_number, first_name, surname)
         self.tax_number = tax_number
-
-        # connection instance to be used later
+        # connection instance to be used later,
+        # rather than inherit from this class
         self.test = DB_Connection()
 
     # Add data to the Customer table using INSERT
@@ -20,21 +22,25 @@ class Customer(People):
                                         ) VALUES (
                                         '{passport_number}','{tax_number}','{first_name}','{surname}',{flight_id},
                                         '{gender}', {boarded_flight});""")
+            # commit the SQL statement to the database
             self.test.connection.commit()
         else:
+            # a message to inform the user what is going on
             print("Customers table does not exist, please try again")
 
     # function to show all the customers
     def show_customers(self):
+        # check if the table is created
         if self.test.cursor.tables(table="Customers", tableType="TABLE").fetchone():
             customers = self.test.cursor.execute("""SELECT * FROM Customers""")
             # for loop to print all the rows of customers
             for rows in customers:
                 print(rows)
         else:
+            # a message to inform the user what is going on
             print("Customers table does not exist, please try again")
 
-#
+# used to ensure these tests only are done when calling from this file
 if __name__ == "__main__":
     customer = Customer("68546354", "Harry", "Potter", "653214")
     customer.add_to_customer_table("68546354", "Harry", "Potter", "653214", "2", "Male", 0)
