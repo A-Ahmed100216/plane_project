fddaf945824c375bda183323fbf23619600c028a
Donovan Gervais <applemac@Donovans-MacBook-Pro-7.local>
2020-11-19 11:13:54 +0000

    Flight trip completed

--- flight_trip.py
+++ flight_trip.py
@@ -1,13 +1,14 @@
-import pyodbc
-import pandas
-from aircraft import Aircraft
+# import pyodbc
+# import pandas
+# from aircraft_class import Aircraft
+from db_connection_class import DB_Connection
 
-class Flight_Trip(Aircraft):
+class Flight_Trip(DB_Connection):
     def __init__(self):
         super().__init__()
 
     def create_table(self):
-        self.cursor.execute("CREATE TABLE Flight_Trip (Flight_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY, craft_id INT, Destination VARCHAR(100), Duration_hrs INT, Scheduled VARCHAR(100));")
+        self.cursor.execute("CREATE TABLE Flight_Trip (Flight_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY, craft_id INT, Destination VARCHAR(100), Duration_hrs INT, Date VARCHAR(100), Time VARCHAR(100));")
 
 
     def view_table(self):
@@ -15,28 +16,27 @@ class Flight_Trip(Aircraft):
         return flights_table
 
     def existing_flights(self):
-        self.cursor.execute("INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Scheduled, flight_time) VALUES (1, 'Marbella', 2, '12/12/2019', '12:00');")
+        self.cursor.execute("INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Date, Time) VALUES (1, 'Marbella', 2, '12/12/2019', '12:00');")
         self.cursor.execute(
-            "INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Scheduled, flight_time) VALUES (2, 'St Lucia', 8, '14/12/2019', '01:00');")
+            "INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Date, Time) VALUES (2, 'St Lucia', 8, '14/12/2019', '01:00');")
         self.cursor.execute(
-            "INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Scheduled, flight_time) VALUES (4, 'Los Angeles', 10, '14/05/2019', '08:00');")
+            "INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Date, Time) VALUES (4, 'Los Angeles', 10, '14/05/2019', '08:00');")
         self.cursor.execute(
-            "INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Scheduled, flight_time) VALUES (3, 'Antigua', 9, '06/06/2019', '10:00');")
+            "INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Date, Time) VALUES (3, 'Antigua', 9, '06/06/2019', '10:00');")
 
         self.connection.commit()
 
     def add_flight(self):
         craft_id = input("Please enter the aircraft ID number for this flight ==> ")
         destination = str(input("Where are you flying to? "))
-        duration = float(input("How many hours is this flight? "))
-        date = str(input("Please enter the date of this flight ==> "))
-        time = str(input("What time is the flight? "))
+        duration = int(input("How many hours is this flight? "))
+        date = str(input("Please enter the date of this flight(dd/mm/yyy) ==> "))
+        time = str(input("What time is the flight?(hh:mm) "))
 
-        self.cursor.execute(f"INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Scheduled) VALUES ('{craft_id}', '{destination}', '{duration}', '{date}', '{time}');")
+        self.cursor.execute(f"INSERT INTO Flight_Trip (craft_id, Destination, Duration_hrs, Date, Time) VALUES ('{craft_id}', '{destination}', '{duration}', '{date}', '{time}');")
 
         self.connection.commit()
 
-        return flights_table
 
 
 
